d28d93edbf29f75663cfa99aa6004b94d4ac6b93
Antoine B <antoine.boulange33@gmail.com>
2018-10-20 19:38:00 +0200

    Fonction jump

--- Classes/Declarations/player.h
+++ Classes/Declarations/player.h
@@ -1,9 +1,9 @@
 #include <ctime>
+#include <cmath>
 #include "position.h"
 
 #ifndef GAMEJAM_1_ENSEIRB_PLAYER_H
 #define GAMEJAM_1_ENSEIRB_PLAYER_H
-
 
 class player {
 private:
@@ -12,8 +12,16 @@ private:
     int _life;
     position _pos;
     position _beginJump;
+    position _mvt;
     bool _toStop;
+    bool _toChangeDirection;
     bool _forward;
+    int _light;
+    bool _reduction;
+    int _timer;
+    int _height;
+    int _width;
+    int _status;
 
 public:
     // Constructors
@@ -25,13 +33,17 @@ public:
     void print_hunger() const;
     void print_life() const;
     bool is_dead() const;
+    void jump();
+    void move();
 
     // Getter
     const position &get_pos() const;
     int get_life() const;
+    const position &get_mvt() const;
 
     // Setter
     void set_pos(int x, int y);
+    void set_mvt(int x, int y);
     void set_life(int life);
 };
 
--- Classes/Definitions/player.cpp
+++ Classes/Definitions/player.cpp
@@ -4,11 +4,21 @@
 #define MAX_HUNGER 100
 #define DEC_HUNGER 20
 #define MAX_LIFE 3
+#define GRAVITY 9.81
+#define PI 3.14
 
-player::player(): _hunger(MAX_HUNGER), _life(MAX_LIFE){}
+player::player(): _hunger{MAX_HUNGER},
+                  _life{MAX_LIFE},
+                  _forward{true},
+                  _reduction{false},
+                  _timer{0},
+                  _height{0},
+                  _width{0},
+                  _status{0}
+{}
 
 // TODO : Déplacer cette fonction dans le controller
-// TODO : Harmoniser la réduction de hunger et celle de life (fonction ou pas ?)
+// TODO : Harmoniser la réduction de _hunger et celle de _life (fonction ou pas ?)
 // TODO : Forker cette fonction pour avoir le "vrai" jeu en parallèle
 void player::hunger_decrease() {
     time_t timer = time(nullptr);
@@ -26,6 +36,67 @@ void player::hunger_decrease() {
             life_decrease();
             print_life();
         }
+    }
+}
+
+void player::move() {
+    int nextX = _pos.get_x() + _mvt.get_x();
+    switch (_status){
+        case 0:
+            if(nextX >= 0 && nextX <= 1200){
+                if(nextX <= 0)
+                    _pos.set_x(0);
+                else if(nextX + _width >= 1200)
+                    _pos.set_x(1200 - _width);
+                else
+                    _pos.set_x(nextX);
+            }
+            break;
+
+        case 1:
+        case 2:
+            jump();
+            break;
+
+        default:
+            break;
+    }
+}
+
+void player::jump(){
+    // TESTS TODO delete
+    _beginJump = _pos;
+    // FIN TESTS
+
+    double theta;
+
+    if(_forward)
+        theta = PI / 3;
+    else
+        theta = 2 * PI / 3;
+
+    auto newX = (int)round(_beginJump.get_x() + ((fabs(_mvt.get_x()) * cos(theta) * _timer) * 0.04));
+
+    if(_reduction)
+        _pos.set_x((int)round((newX) - ((newX - _pos.get_x()) * 0.9)));
+    else
+        _pos.set_x(newX);
+
+    _pos.set_y((int)round((_beginJump.get_y() - _height/2) - ((((-1) * GRAVITY * (_timer * _timer) / 2000) + (_mvt.get_y() * sin(theta) * _timer) + (_height / 2)) * 0.08)));
+
+    _timer += 100;
+
+    if(_pos.get_x() < 0 || _pos.get_x() > 1200 - _width){
+        if(_pos.get_x() < 0)
+            _pos.set_x(0);
+        if(_pos.get_x() > 1200 - _width)
+            _pos.set_x(1200 - _width);
+    }
+
+    if((_pos.get_y() + _height) > 500){
+        _pos.set_y(500 - _height);
+        _timer = 0;
+        _status = 0;
     }
 }
 
@@ -60,4 +131,13 @@ int player::get_life() const {
 
 void player::set_life(int life) {
     this->_life = life;
+}
+
+const position &player::get_mvt() const {
+    return this->_mvt;
+}
+
+void player::set_mvt(int x, int y) {
+    this->_mvt.set_x(x);
+    this->_mvt.set_y(y);
 }
